96aa1e6df7ce25779e7cc085f0cc978b08ab9db5
Matt Keys <MatthewjKeys22@gmail.com>
2021-11-06 15:17:21 -0600

    fuck this project

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const promptUser = () => {
-    return inquirer.prompt([
+const questions = [
         {
             type: 'input',
-            name: 'name',
+            name: 'title',
             message: 'What is the title of your project? (Required)',
             validate: projectNameInput => {
                 if (projectNameInput) {
@@ -46,7 +47,7 @@ const promptUser = () => {
             {
             type: 'input',
             name: 'usage',
-            message: 'Please enter the usage information for your project: (Required)',
+            message: 'Please enter the usage information for your project - instructions for use, screenshots, etc.  : (Required)',
             validate: usageInput => {
                 if (usageInput) {
                 return true;
@@ -56,44 +57,98 @@ const promptUser = () => {
                 }
             }
             },
-            {
-            type: 'input',
-            name: 'contribution',
-            message: 'What are the contribution guidlines of your project? (Required)',
-            validate: contributionInput => {
-                if (contributionInput) {
-                return true;
-                } else {
-                console.log("Please enter your projects' contribution guidelines!");
-                return false;
-                }
-            }
-            },
+            // {
+            // type: 'input',
+            // name: 'contribution',
+            // message: 'What are the contribution guidelines of your project? (Required)',
+            // validate: contributionInput => {
+            //     if (contributionInput) {
+            //     return true;
+            //     } else {
+            //     console.log("Please enter your projects' contribution guidelines!");
+            //     return false;
+            //     }
+            // }
+            // },
             {
             type: 'input',
             name: 'tests',
-            message: 'What are the testing instructinos for your project? (Required)',
+            message: 'What are the testing instructions for your project? (Required)',
             validate: testInput => {
                 if (testInput) {
                 return true;
                 } else {
-                console.log('Please enter your project test instructions:');
+                console.log('Please enter your project test instructions!');
                 return false;
                 }
             }
             },
-    ]);
-} ;
+            {
+                type: 'input',
+                name: 'username',
+                message: 'What is your GitHub username? (Required)',
+                validate: testInput => {
+                    if (testInput) {
+                    return true;
+                    } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                    }
+                }
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: 'What is your email address? (Required)',
+                    validate: testInput => {
+                        if (testInput) {
+                        return true;
+                        } else {
+                        console.log('Please enter your email!');
+                        return false;
+                        }
+                    }
+                    },
+                    {
+                        type: 'checkbox',
+                        name: 'license',
+                        message: 'What licenses would you like to apply to your project? (Required)',
+                        choices: ['MIT', 'Apache', 'GTL'],
+                        validate: licenseInput => {
+                            if (licenseInput) {
+                            return true;
+                            } else {
+                            console.log('Please select at least one license!');
+                            return false;
+                            }
+                        }
+                        },
+                    
+
+        ];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+// return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+return fs.writeFile(fileName, data, err => {
+    if (err) {
+        console.log(err);
+    }
+    return generateMarkdown(data);
+
+})
+
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    promptUser()
-        .then( answersObject => {
-            console.log(answersObject)
-        })
+    inquirer.prompt(questions).then(data => {
+            console.log('Generate README...');
+            console.log(data);
+            writeToFile ('README.md', generateMarkdown(data));
+        });
+        
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
+const contributionGuidelines = require('./covenant-guidelines.js')
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,8 +15,42 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
 
+  ## **Table of Contents**
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage Information](#usage-information)
+  * Contribution Guidelines](#contribution-guidelines)
+  * [Tests](#tests)
+
+
+  ## **Description**
+  ${data.description}
+
+
+  ## **Installation** 
+  To run this program locally, ${data.installation}
+  You can also watch a short video on installation [here.](https://${data.link})
+
+
+  ## **Usage Information**
+  ${data.usage}
+
+
+  ## **Contribution Guidelines** 
+  ${contributionGuidelines}
+    
+
+  ## **Tests** 
+  ${data.tests}
+
+  ## **Questions**
+  Visit my [GitHub!](https://${data.username}.github.io)
+  Or, if you'd like to contact me directly, [email me!](mailto:${data.email})
 `;
 }
 
 module.exports = generateMarkdown;
+
+
